f2f26e366a1057298b110e8ae5d3c178af8c7166
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2023-12-05 21:32:52 -0300

    Cria estoque conforme a aula

--- estoque.js
+++ estoque.js
@@ -1,3 +1,4 @@
+/*
 let estoque = {
     'joao': {
         'maca': 1,
@@ -7,7 +8,17 @@ let estoque = {
         'maca': 2,
         'pera': 3
     }
-};
+};*/
+let estoque = {
+    'joao': [
+        {'tipo': 'maca', 'quantidade': 1},
+        {'tipo': 'peras', 'quantidade': 2}
+    ],
+    'maria': [
+        {'tipo': 'maca', 'quantidade': 2},
+        {'tipo': 'banana', 'quantidade': 4}
+    ]
+}
 
 function getEstoque(){
     return structuredClone(estoque);
